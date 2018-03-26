8f52c45153dbf411589febc64c9f82c176498cba
wangyj <wangyj@liepin.com>
2018-03-26 16:02:06 +0800

    增加 pyecharts demo

---
+++ examples/pyecharts/pyecharts.py
@@ -0,0 +1,12 @@
+from pyecharts import Bar, Line
+from pyecharts.engine import create_default_environment
+
+bar = Bar("我的第一个图表", "这里是副标题")
+bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
+
+line = Line("我的第一个图表", "这里是副标题")
+line.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
+
+bar 
+
+line
